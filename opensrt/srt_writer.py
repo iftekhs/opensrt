4959ba4ee2327fe876451c4699a2e9e431f28804
iftekhs <iftekhstudio@gmail.com>
2026-03-25 06:00:59 +0600

    removed tag

--- opensrt/srt_writer.py
+++ opensrt/srt_writer.py
@@ -3,5 +3,11 @@ import stable_whisper
 
 
 def write_srt(result: stable_whisper.WhisperResult, output_path: str, karaoke: bool = False) -> None:
-    result.to_srt_vtt(output_path, vtt=False, karaoke=karaoke)
+    if karaoke:
+        result.to_srt_vtt(output_path, vtt=False)
+    else:
+        result.to_srt_vtt(output_path, vtt=False)
     print(f"Subtitles written to: {os.path.abspath(output_path)}")
+
+
+# tag=('<font color="#00ff00">', '</font>')
